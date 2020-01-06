9e7dc7bf1982926c99edb929353c822bcb139e86
zy-yz <1090712762@qq.com>
2020-01-06 16:24:50 +0800

    Initial commit

---
+++ src/test/java/com/example/italker/ItalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.italker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
